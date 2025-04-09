77d84462b7c67cd6124eac05421aed3775e9d871
AndrewMaraiev <105721308+AndrewMaraiev@users.noreply.github.com>
2025-04-09 15:19:26 +0300

    fix timeslot error

--- trg_marketing/trg_marketing/doctype/appointment/appointment.py
+++ trg_marketing/trg_marketing/doctype/appointment/appointment.py
@@ -4,32 +4,36 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
-from frappe.utils import get_datetime
+from frappe.utils import get_datetime, get_time
 
 class Appointment(Document):
     def validate(self):
         self.validate_time_slot()
 
     def validate_time_slot(self):
-        """Validate that the time matches the selected time slot"""
+        """Validate that the time falls within the selected time slot range"""
         time_slots = {
-            "9am - 12pm": "09:00:00",
-            "10am - 1pm": "10:00:00",
-            "11am - 2pm": "11:00:00",
-            "12pm - 3pm": "12:00:00",
-            "1pm - 4pm": "13:00:00",
-            "2pm - 5pm": "14:00:00",
-            "3pm - 6pm": "15:00:00",
-            "4pm - 7pm": "16:00:00",
-            "5pm - 8pm": "17:00:00",
-            "6pm - 9pm": "18:00:00",
-            "7pm - 10pm": "19:00:00"
+            "9am - 12pm": ("09:00:00", "12:00:00"),
+            "10am - 1pm": ("10:00:00", "13:00:00"),
+            "11am - 2pm": ("11:00:00", "14:00:00"),
+            "12pm - 3pm": ("12:00:00", "15:00:00"),
+            "1pm - 4pm": ("13:00:00", "16:00:00"),
+            "2pm - 5pm": ("14:00:00", "17:00:00"),
+            "3pm - 6pm": ("15:00:00", "18:00:00"),
+            "4pm - 7pm": ("16:00:00", "19:00:00"),
+            "5pm - 8pm": ("17:00:00", "20:00:00"),
+            "6pm - 9pm": ("18:00:00", "21:00:00"),
+            "7pm - 10pm": ("19:00:00", "22:00:00")
         }
 
         if self.time_slot and self.time:
-            expected_time = time_slots.get(self.time_slot)
-            if expected_time and str(self.time) != expected_time:
-                frappe.throw(f"Time {self.time} does not match the selected time slot {self.time_slot}")
+            start_time, end_time = time_slots.get(self.time_slot)
+            current_time = get_time(self.time)
+            slot_start = get_time(start_time)
+            slot_end = get_time(end_time)
+
+            if not (slot_start <= current_time <= slot_end):
+                frappe.throw(f"Time {self.time} is not within the selected time slot {self.time_slot}")
 
     @frappe.whitelist()
     def get_events(start, end, filters=None):
