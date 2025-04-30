18a4f9e0e7e875771966e69958e0940e0013cffc
Andrew Pavlichuk <andrewpavlicuk2@gmail.com>
2025-04-30 13:56:44 +0000

    Partially fixed sort arrows appearing in the wrong page

--- trg_marketing/trg_marketing/doctype/appointment/appointment.js
+++ trg_marketing/trg_marketing/doctype/appointment/appointment.js
@@ -1,41 +1,7 @@
-(function anonymous() {
-
-	frappe.ui.form.on('Appointment', {
-		refresh(frm) {
-			console.log("test")
-			$(".control-input:has(select) use[href='#icon-select']")
-				.attr("href", "#icon-arrow-down-trg")
-		}
-	})
-
-	function applySortArrows(sortBy = "", sortOrder = "") {
-		sortBy = sortBy.toLowerCase();
-		sortOrder = sortOrder.toLowerCase();
-
-		$('.list-row-col:has(>[data-sort-by])').not(':has(.sort-icon)').not(':has(>[data-sort-by="name"])').each((_, el) => {
-			const $col = $(el).find('[data-sort-by]');
-			const field = $col.attr('data-sort-by').toLowerCase();
-
-			const href = (field === sortBy)
-				? (sortOrder === "asc" ? "#icon-arrow-3-4-trg" : "#icon-arrow-3-3-trg")
-				: "#icon-arrow-3-trg";
-
-			const svg = $(`<svg class="sort-icon" aria-hidden="true"><use href="${href}"></use></svg>`);
-			$(el).append(svg);
-		});
+frappe.ui.form.on('Appointment', {
+	refresh(frm) {
+		console.log("test")
+		$(".control-input:has(select) use[href='#icon-select']")
+			.attr("href", "#icon-arrow-down-trg")
 	}
-
-	frappe.listview_settings["Appointment"] = {
-		onload(listview) {
-			frappe.after_ajax(() => {
-				const wrapper = listview.page.wrapper;
-
-				$(document).ajaxComplete(function (event, xhr, settings) {
-					if (settings && settings.url && settings.url.includes('frappe.desk.reportview.get') && settings.type === "GET") {
-						applySortArrows(listview.sort_by, listview.sort_order);
-					}
-				});
-			});
-		}
-	};
 })
