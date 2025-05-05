4c4fbb8ac60273c8f0d0d8a718d2940d4221b1e3
Andrew Pavlichuk <andrewpavlicuk2@gmail.com>
2025-05-05 12:42:46 +0000

    Added icon for save button in FAQ form

--- trg_marketing/trg_marketing/doctype/faq_acticles/faq_acticles.js
+++ trg_marketing/trg_marketing/doctype/faq_acticles/faq_acticles.js
@@ -3,6 +3,12 @@
 
 frappe.ui.form.on("FAQ Acticles", {
 	refresh(frm) {
+		$(`.btn[data-label="Save"]`).prepend($(`
+			<svg class="save-icon" aria-hidden="true">
+				<use href="#icon-tick-circle-trg"></use>
+			</svg>
+		`));
+
 		if (!frm.is_new()) {
 			frm.add_custom_button(__("Compare Versions"), () => {
 				new frappe.ui.DiffView("FAQ Acticles", "content", frm.doc.name);
